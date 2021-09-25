912e02a738d310bd6b74811e74ac2c6dcdac4eaf
Amr Ahmed <amr.magdy.ahmed92@gmail.com>
2021-09-25 14:18:24 +0200

    Edited the result in ACL controller

--- server/Controllers/ACL/ACL.js
+++ server/Controllers/ACL/ACL.js
@@ -9,26 +9,29 @@ module.exports = {
 
         if (tokenType != 'Bearer') {
             return result = {
-                errorCode: 403,
-                errorDescription: 'invalid_token_type',
-                errorMessage: 'Authorization header must be like: (Bearer access-token)'
+                status: 'error',
+                code: 403,
+                description: 'invalid_token_type',
+                message: 'Authorization header must be like: (Bearer access-token)'
             };
         };
 
         var access = ACLDBMethods.getAccessToken(accessToken);
         if (!access || !access.access_token || !access.client_id || !access.expired_at || !access.user_id) {
             return result = {
-                errorCode: 403,
-                errorDescription: 'invalid_access_token',
-                errorMessage: 'Invalid access token'
+                status: 'error',
+                code: 403,
+                description: 'invalid_access_token',
+                message: 'Invalid access token'
             };
         };
 
         if (Date.now > Date.parse(access.expired_at)) {
             return result = {
-                errorCode: 401,
-                errorDescription: 'expired_access_token',
-                errorMessage: 'Access token has expired'
+                status: 'error',
+                code: 401,
+                description: 'expired_access_token',
+                message: 'Access token has expired'
             };
         };
 
