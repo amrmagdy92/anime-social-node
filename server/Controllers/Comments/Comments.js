352b79c81d78ad483b9db032dd70d8880673babc
Amr Ahmed <amr.magdy.ahmed92@gmail.com>
2021-09-27 01:08:41 +0200

    Created the getComments function in the CommentsController

--- server/Controllers/Comments/Comments.js
+++ server/Controllers/Comments/Comments.js
@@ -1,7 +1,63 @@
 const commentsDBMethods = require('./CommentsMySQL');
+const AccessController = require('../ACL/ACL');
 
 module.exports = {
-    getComments: () => {},
+    getComments: (authorization, params) => {
+        var checkAuthorization = AccessController.isAuthorizedUSer(authorization);
+        if (checkAuthorization.status == 'error') {
+            return checkAuthorization;
+        };
+
+        var commentReferenceID = params.comment_reference_id.trim();
+        var commentReferenceType = params.comment_reference_type.trim();
+        var validationErrors;
+
+        if (!commentReferenceID) {
+            validationErrors.commentReferenceID = {
+                reason: 'empty',
+                message: 'Reference ID can not be empty'
+            };
+        };
+
+        var commentReferenceTypes = ['posts'];
+        if (!commentReferenceTypes.includes(commentReferenceType)) {
+            validationErrors.commentReferenceType = {
+                reason: 'in_array',
+                message: `Reference type must be: ${commentReferenceTypes.join(', ')}`
+            };
+        };
+
+        if (!validationErrors.hasOwnProperty(commentReferenceID) && validationErrors.hasOwnProperty(commentReferenceType)) {
+            var reference = commentsDBMethods.getReferenceObject(commentReferenceID, commentReferenceType);
+            if (!reference) {
+                validationErrors.commentReferenceID = {
+                    reason: 'invalid_reference_id',
+                    message: `Reference ID not found for ${commentReferenceType}`
+                };
+            };
+        };
+
+        if (validationErrors) {
+            return result = {
+                status: 'error',
+                code: 422,
+                reason: 'validation_errors',
+                message: 'Please correct the highlighted errors',
+                validationErrors
+            };
+        };
+        params._order_by = 'comment_created_at_desc';
+        params._records_per_page = 25;
+
+        var data = commentsDBMethods.getComments(params);
+
+        return result = {
+            status: 'success',
+            code: 200,
+            message: 'Found the queried data',
+            queryResult: data
+        }
+    },
     createComment: () => {},
     updateComment: () => {},
     deleteComment: () => {},
