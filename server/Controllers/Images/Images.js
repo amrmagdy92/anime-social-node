7d437c913e2d37f0db87ab75b518376ef07f6052
Amr Ahmed <amr.magdy.ahmed92@gmail.com>
2021-10-03 15:45:53 +0200

    PAdded a utility file and partially created the createImage functionin ImagesController

--- server/Controllers/Images/Images.js
+++ server/Controllers/Images/Images.js
@@ -1,6 +1,87 @@
 const imagesDBMethods = require('./ImagesMySQL');
+const AccessController = require('../ACL/ACL');
+const path = require('path');
+const fs = require('fs');
+const uniqueID = require('../../Misc/utility');
 
 module.exports = {
-    createImages: () => {},
+    createImages: (authorization, imageReferenceType, imageFiles) => {
+        var checkAuthorization = AccessController.isAuthorizedUser(authorization);
+        if (checkAuthorization.status == 'error') {
+            return checkAuthorization;
+        };
+
+        var validationErrors;
+        const imageReferenceTypes = ['posts'];
+
+        if (!imageReferenceTypes.includes(imageReferenceType)) {
+            validationErrors.image_reference_type = {
+                reason: 'in_array',
+                message: `Reference type must be: ${imageReferenceTypes.join(',')}`
+            };
+        };
+
+        if (typeof imageFiles != 'object') {
+            validationErrors.image_files = {
+                reason: 'is_json',
+                message: 'List of images must be JSON.'
+            };
+        };
+
+        if (!validationErrors.hasOwnProperty('image_files')) {
+            if (imageReferenceType == 'posts' && Object.keys(imageFiles).length > maxLength) {
+                validationErrors.image_files = {
+                    reason: 'max_limit_exceeded',
+                    message: `A maximum of ${process.env.MAX_IMAGES_SINGLE_POST} is allowed in a single post`
+                };
+            };
+
+            Object.entries(imageFiles).forEach(([key, value]) => {
+                if (value == null || value == undefined) {
+                    validationErrors.imageFiles = {
+                        reason: 'empty',
+                        message: `Please select image file. The image number: ${key} is empty`
+                    };
+                };
+                break;
+            });
+
+            Object.entries(imageFiles).forEach(([key, value]) => {
+                // TODO: check file size here
+                // TODO: shouldn't file size check be a front end job?
+            });
+
+            Object.entries(imageFiles).forEach(([key, value]) => {
+                var extension = path.extname(value);
+                if (!process.env.ALLOWED_IMAGE_EXTENSIONS.includes(extension)) {
+                    validationErrors.image_files = {
+                        reason: 'invalid_file_type',
+                        message: `The image number ${key} has an invalid extension. Only use images with extensions: ${process.env.ALLOWED_IMAGE_EXTENSIONS.join(',')}`
+                    };
+                };
+                break;
+            });
+
+            // TODO: Add lines 129-139 from ImagesService.php
+        };
+
+        if(validationErrors) {
+            return result = {
+                status: 'error',
+                code: 422,
+                reason: 'validation_errors',
+                message: 'Please correct the highlighted errors',
+                validationErrors
+            };
+        };
+
+        var image_ids = [];
+        Object.entries(imageFiles).forEach(([key, value]) => {
+            var extension = path.extname(value);
+            var baseName = `${new Date().toISOString().split('T')[0]}-${uniqueID(8)}`;
+
+            // TODO: try to understand the logic from the PHP file
+        });
+    },
     deleteImage: () => {}
 };
---
+++ server/Misc/utility.js
@@ -0,0 +1,63 @@
+module.exports = {
+    getUploadErrorMessage: (code) => {
+        switch(code) {
+            case UPLOAD_ERR_INI_SIZE:
+                return result = {
+                    reason: 'upload_err_ini_size',
+                    message: 'The uploaded file exceeds the UPLOAD_MAX_FILESIZE'
+                };
+            case UPLOAD_ERR_FORM_SIZE:
+                return result = {
+                    reason: 'upload_err_form_size',
+                    message: 'The uploaded file exceeds the MAX_FILE_SIZE'
+                };
+            case UPLOAD_ERR_PARTIAL:
+                return result = {
+                    reason: 'upload_err_partial',
+                    message: 'The uploaded file was only partially uploaded'
+                };
+            case UPLOAD_ERR_NO_FILE:
+                return result = {
+                    reason: 'upload_err_no_file',
+                    message: 'No file was uploaded'
+                };
+            case UPLOAD_ERR_NO_TMP_DIR:
+                return result = {
+                    reason: 'upload_err_no_tmp_dir',
+                    message: 'Missing a temporary folder'
+                };
+            case UPLOAD_ERR_CANT_WRITE:
+                return result = {
+                    reason: 'upload_err_cant_write',
+                    message: 'Failed to write file to disk.'
+                };
+            case UPLOAD_ERR_EXTENSION:
+                return result = {
+                    reason: 'upload_err_extension',
+                    message: 'Invalid uploaded file extension.'
+                };
+        }
+    },
+    readableBytes: (bytes) => {
+        var sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+        var index = Math.floor(Math.log(bytes) / Math.log(1024));
+
+        return `${bytes / Math.pow(1024, index)} ${sizes[index]}`;
+    },
+    generateUniqueId: (length) => {
+        var intLength = parseInt(length) / 2;
+
+        if (intLength <= 0) {
+            throw new Error('Length must be greater than 0');
+        };
+        
+        var uniqueID = '';
+        const characters = '0123456789abcdef';
+        for (let i = 0; i < (intLength * 2); i++) {
+            uniqueID += characters[Math.floor((Math.random() * characters.length) - 1)];
+        };
+
+        return uniqueID;
+    },
+    parseURL: () => {}
+}
