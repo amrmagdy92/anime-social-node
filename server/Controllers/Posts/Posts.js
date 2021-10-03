a409a312b4758450b051fa9c7e2a5c1b108a6bb8
Amr Ahmed <amr.magdy.ahmed92@gmail.com>
2021-10-03 22:54:15 +0200

    Added createPost method logic in PostsController

--- server/Controllers/Posts/Posts.js
+++ server/Controllers/Posts/Posts.js
@@ -1,8 +1,188 @@
 const postsDBMethods = require('./PostsMySQL');
 const AccessController = require('../ACL/ACL');
+const ImagesController = require('../Images/Images');
+const ImagesDBMethods = require('../Images/ImagesMySQL');
 
 module.exports = {
-    createPost: () => {},
+    createPost: (authorization, postText, postScope, postIsSpoiler, animeID, postYoutubeURL, imageIDs, pollLength, choicesData, postType, imageFiles) => {
+        var checkAuthorization = AccessController.isAuthorizedUser(authorization);
+        if (checkAuthorization.status == 'error') {
+            return checkAuthorization;
+        };
+
+        var authorized = checkAuthorization.access;
+        var authorID = authorized.user_id;
+        var postsCountToday = postsDBMethods.getPostsCountForToday(authorID, postScope);
+        
+        if (postScope == 'Public' && postsCountToday >= authorized.user.user_per_day_limit_for_public_posts) {
+            return result = {
+                status: 'error',
+                code: 400,
+                reason: 'public_posts_limit_exceeded',
+                message: `A maximum of ${authorized.user.user_per_day_limit_for_public_posts} is allowed everyday.`
+            };
+        };
+
+        if (postScope == 'Following' && postsCountToday >= authorized.user.user_per_day_limit_for_following_posts) {
+            return result = {
+                status: 'error',
+                code: 400,
+                reason: 'following_posts_limit_exceeded',
+                message: `A maximum of ${authorized.user.user_per_day_limit_for_following_posts} is allowed everyday.`
+            };
+        };
+
+        var objects = 0;
+        if (postYoutubeURL) objects++;
+        if (imageIDs) objects++;
+        if (choicesData) objects++;
+
+        if (objects > 1) {
+            return result = {
+                status: 'error',
+                code: 400,
+                reson: 'objects_exceeded',
+                message: 'Only one object allowed at a time choose either embedded URL, images or video'
+            };
+        };
+
+        if (!postType) {
+            return result = {
+                status: 'error',
+                code: 400,
+                reason: 'post_type',
+                message: 'post_type is empty'
+            };
+        };
+
+        var validationErrors;
+
+        if (!postText) {
+            validationErrors.post_text = {
+                reason: 'empty',
+                message: 'Please enter post text'
+            };
+        };
+
+        if (postText && postText.length > 2000) {
+            validationErrors.post_text = {
+                reason: 'strlen',
+                message: 'Please enter less than 2000 char.'
+            };
+        };
+
+        var postScopes = ['Public', 'Following'];
+        if (!postScopes.includes(postScope)) {
+            validationErrors.post_scope = {
+                reason: 'in_array',
+                message: `Please slect from: ${postScopes.join(',')}.`
+            };
+        };
+
+        var postIsSpoilerValues = ['Yes', 'No'];
+        if (!postIsSpoilerValues.includes(postIsSpoiler)) {
+            validationErrors.post_is_spoiler = {
+                reason: 'in_array',
+                message: `Please select from: ${postIsSpoilerValues.join(',')}.`
+            };
+        };
+
+        if (animeID) {
+            var anime = postsDBMethods.getAnimeByID(animeID);
+            var key;
+
+            if (authorized.platform == 'iOS') {
+                key = 'authorized_ios_published';
+            } else if (authorized.platform == 'android') {
+                key = 'anime_android_published';
+            };
+
+            if (anime.key != 'Published') {
+                validationErrors.anime_id = {
+                    reason: 'invalid_anime',
+                    message: 'Please select a valid anime'
+                };
+            };
+        }
+
+        if (imageFiles) {
+            var image_IDs = ImagesController.createImages(authorization, 'posts', imageFiles);
+            if (typeof image_IDs != 'object') {
+                validationErrors.image_ids = {
+                    reason: 'is_json',
+                    message: 'Image list must be an array'
+                };
+            };
+
+            if (!validationErrors.hasOwnProperty('image_ids')) {
+                if (Set(imageIDs).size != imageIDs.length) {
+                    validationErrors.image_ids = {
+                        reason: 'duplicate',
+                        message: 'Duplicate image found.'
+                    };
+                };
+            };
+
+            if (!validationErrors.hasOwnProperty('image_ids')) {
+                if (image_IDs.length != process.env.MAX_IMAGES_SINGLE_POST) {
+                    validationErrors.image_ids = {
+                        reason: 'max_limit_reached',
+                        message: `A maximum of ${process.env.MAX_IMAGES_SINGLE_POST} images is allowed.`
+                    };
+                };
+            };
+
+            if (!validationErrors.hasOwnProperty('image_ids')) {
+                Object.keys(image_IDs).forEach(([key, value]) => {
+                    var image = ImagesDBMethods.getImageByID(key);
+                    if (!image) {
+                        validationErrors.image_ids = {
+                            reason: 'invalid_image',
+                            message: 'Please select valid image.'
+                        };
+                    };
+                });
+            };
+        };
+
+        if (validationErrors) {
+            return result = {
+                status: 'error',
+                code: 422,
+                reason: 'validation_errors',
+                message: 'Please correct highlighted errors.',
+                validationErrors
+            };
+        };
+        var post_id = postsDBMethods.insertPost(authorID, postText, postScope, postIsSpoiler, postYoutubeURL,postType);
+
+        if (animeID) {
+            postsDBMethods.insertPostAnime(post_id, animeID);
+        };
+
+        if (choicesData) {
+            var poll_id = postsDBMethods.insertPoll(post_id, pollLength, authorID);
+            Object.keys(choicesData).forEach(([key, value]) => {
+                var anime_id = choicesData[key].anime_id ? choicesData[key].anime_id : null;
+                var characters_id = choicesData[key].characters_id ? choicesData[key].characters_id : null;
+                postsDBMethods.insertChoice(poll_id, choicesData[key].choice_title, anime_id, characters_id);
+            });
+        };
+
+        if (imageIDs) {
+            Object.keys(imageIDs).forEach(([key, value]) => {
+                postsDBMethods.insertPostImage(post_id, imageIDs[key]);
+            });
+        };
+
+        // TODO: Add insert video
+
+        return result = {
+            status: 'success',
+            code: 200,
+            message: 'Post created successfully'
+        };
+    },
     updatePost: () =>{},
     deletePost: () => {},
     hide: () => {},
