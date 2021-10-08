cb8ce76175d7e56297ee772304275a7c079738d4
Amr Ahmed <amr.magdy.ahmed92@gmail.com>
2021-10-08 02:34:50 +0200

    changed imports and configuration to use .env file

--- server/devBundle.js
+++ server/devBundle.js
@@ -1,11 +1,11 @@
-import config from '../config/config';
-import webpack from 'webpack';
-import webpackMiddleware from 'webpack-dev-middleware';
-import webpackHotMiddleware from 'webpack-hot-middleware';
-import webpackConfig from '../webpack.config.client';
+require('dotenv').config();
+const webpack = require('webpack');
+const webpackMiddleware = require('webpack-dev-middleware');
+const webpackHotMiddleware = require('webpack-hot-middleware');
+const webpackConfig = require('../webpack.config.client');
 
 const compile = (app) => {
-    if (config.env == "development") {
+    if (process.env.NODE_ENV == "development") {
         const compiler = webpack(webpackConfig);
         const middleware = webpackMiddleware(compiler,{
             publicPath: webpackConfig.output.path
