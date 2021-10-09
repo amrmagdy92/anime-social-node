b4611b6726d4c4a3d3c152b5f9f1515bba6c9f28
Amr Ahmed <amr.magdy.ahmed92@gmail.com>
2021-10-09 15:24:32 +0200

    Finalized PollsRoute with its logic

--- server/Routes/PollsRoute.js
+++ server/Routes/PollsRoute.js
@@ -2,11 +2,21 @@ var router = require('express').Router();
 const PollsController = require('../Controllers/Polls/Polls');
 
 router.post('/vote', (req, res) => {
-    PollsController.addVote();
+    res.json(PollsController.addVote(
+        req.headers.authorization,
+        req.body.post_id,
+        req.body.poll_id,
+        req.body.choice_id
+    ));
 });
 
 router.post('/create', (req, res) => {
-    res.json(PollsController.createPoll());
+    res.json(PollsController.createPoll(
+        req.headers.authorization,
+        req.body.poll_length,
+        req.body.post_id,
+        req.body.choices_data
+    ));
 })
 
 module.exports = router;
