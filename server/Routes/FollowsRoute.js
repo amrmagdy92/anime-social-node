525ecb3c950458823d8a7a349a859e1fa9420613
Amr Ahmed <amr.magdy.ahmed92@gmail.com>
2021-10-09 15:20:04 +0200

    Finalized FollowsRoute with its logic

--- server/Routes/FollowsRoute.js
+++ server/Routes/FollowsRoute.js
@@ -2,28 +2,52 @@ var router = require('express').Router();
 var FollowController = require('../Controllers/Follows/Follows');
 
 router.post('/follow', (req, res) => {
-    FollowController.follow();
+    res.json(FollowController.follow(
+        req.headers.authorization,
+        req.body.following_id
+    ));
 });
 router.post('/unfollow', (req, res) => {
-    FollowController.unfollow();
+    res.json(FollowController.unfollow(
+        req.headers.authorization,
+        req.body.following_id
+    ));
 });
 router.post('/block-user', (req,res) => {
-    FollowController.blockUser();
+    res.json(FollowController.blockUser(
+        req.headers.authorization,
+        req.body.blocked_user_id
+    ));
 });
 router.post('/unblock-user', (req, res) => {
-    FollowController.unblockUser()
+    res.json(FollowController.unblockUser(
+        req.headers.authorization,
+        req.body.blocked_user_id
+    ));
 });
 router.get('/search-followings', (req, res) => {
-    FollowController.getSearchFollowings();
+    res.json(FollowController.getSearchFollowings(
+        req.headers.authorization,
+        req.params
+    ));
 });
 router.get('/followings', (req, res) => {
-    FollowController.getFollowings();
+    res.json(FollowController.getFollowings(
+        req.headers.authorization,
+        req.params
+    ));
 });
 router.get('/followers', (req, res) => {
-    FollowController.getFollowers();
+    res.json(FollowController.getFollowers(
+        req.headers.authorization,
+        req.params
+    ));
 });
 router.get('/blocked-users', (req, res) => {
-    FollowController.getBlockedUsers();
+    res.json(FollowController.getBlockedUsers(
+        req.headers.authorization,
+        req.params
+    ));
 });
 
 module.exports = router;
