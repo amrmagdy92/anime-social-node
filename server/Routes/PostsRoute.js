6e1d045a893817e178739552f2e5df9d761aeb1c
Amr Ahmed <amr.magdy.ahmed92@gmail.com>
2021-10-09 15:35:14 +0200

    Finalized PostsRoute with its logic

--- server/Routes/PostsRoute.js
+++ server/Routes/PostsRoute.js
@@ -2,22 +2,52 @@ var router = require('express').Router();
 const PostsController = require('../Controllers/Posts/Posts');
 
 router.get('/', (req, res) => {
-    PostsController.getPosts();
+    res.json(PostsController.getPosts(
+        req.headers.authorization,
+        req.params
+    ));
 });
 router.post('/create', (req, res) => {
-    PostsController.createPost();
+    res.json(PostsController.createPost(
+        req.headers.authorization,
+        req.body.post_text,
+        req.body.post_scope,
+        req.body.post_is_spoiler,
+        req.body.anime_id,
+        req.body.post_youtube_url,
+        req.body.image_ids,
+        req.body.poll_length,
+        req.body.choices_data,
+        req.body.post_type,
+        req.body.image_files
+    ));
 });
 router.post('/hide', (req, res) => {
-    PostsController.hide();
+    res.json(PostsController.hide(
+        req.headers.authorization,
+        req.body.post_id
+    ));
 });
 router.post('/reaction', (req, res) => {
-    PostsController.reaction();
+    res.json(PostsController.reaction(
+        req.headers.authorization,
+        req.body.post_id,
+        req.body.reaction
+    ));
 });
 router.post('/update/:post_id', (req, res) => {
-    PostsController.updatePost();
+    res.json(PostsController.updatePost(
+        req.headers.authorization,
+        req.params.post_id,
+        req.body.post_text,
+        req.body.post_is_spoiler
+    ));
 });
 router.delete('/update/:post_id', (req, res) => {
-    PostsController.deletePost();
+    res.json(PostsController.deletePost(
+        req.headers.authorization,
+        req.body.post_id
+    ));
 });
 
 module.exports = router;
