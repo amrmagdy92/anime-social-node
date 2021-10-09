3740a8c1fe1052baf60f388f3b6e9b347016b791
Amr Ahmed <amr.magdy.ahmed92@gmail.com>
2021-10-09 14:52:43 +0200

    Finalized FlagsRoute with its logic

--- server/Routes/FlagsRoute.js
+++ server/Routes/FlagsRoute.js
@@ -2,7 +2,13 @@ var router = require('express').Router();
 const FlagsController = require('../Controllers/Flags/Flags');
 
 router.post('/create', (req, res) => {
-    FlagsController.createFlag();
+    res.json(FlagsController.createFlag(
+        req.headers.authorization,
+        req.body.flag_reference_id,
+        req.body.flag_reference_type,
+        req.body.flag_reason_id,
+        req.body.flag_explanation
+    ));
 });
 
 module.exports = router;
