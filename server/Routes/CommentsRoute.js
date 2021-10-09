3060e92de64a9d228429a0cf9d8f8a98e7bee864
Amr Ahmed <amr.magdy.ahmed92@gmail.com>
2021-10-09 14:48:59 +0200

    Finalized CommentsRoute with its logic

--- server/Routes/CommentsRoute.js
+++ server/Routes/CommentsRoute.js
@@ -2,19 +2,39 @@ var router = require('express').Router();
 const CommentsController = require('../Controllers/Comments/Comments');
 
 router.get('/', (req, res) => {
-    CommentsController.getComments();
+    res.json(CommentsController.getComments(req.headers.authorization, req.params));
 });
 router.post('/create', (req, res) => {
-    CommentsController.createComment();
+    res.json(CommentsController.createComment(
+        req.headers.authorization,
+        req.body.comment_reference_id,
+        req.body.comment_reference_type,
+        req.body.comment_parent_id,
+        req.body.comment_text,
+        req.body.comment_is_spoiler,
+        req.body.comment_reply_to_user_id
+    ));
 });
 router.post('/reaction', (req, res) => {
-    CommentsController.reaction();
+    res.json(CommentsController.reaction(
+        req.headers.authorization,
+        req.body.comment_id,
+        req.body.reaction
+    ));
 });
 router.post('/update/:comment_id', (req, res) => {
-    CommentsController.updateComment();
+    res.json(CommentsController.updateComment(
+        req.headers.authorization,
+        req.body.comment_id,
+        req.body.comment_text,
+        req.body.comment_is_spoiler
+    ));
 });
 router.delete('/delete/:comment_id', (req, res) => {
-    CommentsController.deleteComment();
+    res.json(CommentsController.deleteComment(
+        req.headers.authorization,
+        req.body.comment_id
+    ));
 });
 
 module.exports = router;
